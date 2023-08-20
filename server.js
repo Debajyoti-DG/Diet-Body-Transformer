703790cc3bcbd7efb8ea9a8d9d3f7e79376a184a
Debajyoti <dattaguptadebajyoti@gmail.com>
2023-08-20 23:34:17 +0530

    Added login popup and beautified roadmap

--- server.js
+++ server.js
@@ -118,3 +118,10 @@ function show_hide() {
         x.style.display = 'none';
     }
 }
+
+document.querySelector("#show-login").addEventListener("click",function(){
+    document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+    document.querySelector(".popup").classList.remove("active");
+});

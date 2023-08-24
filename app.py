85144081d8a7de50a0e8e9502ba4d4a622fdba30
Debajyoti Datta Gupta <dattaguptadebajyoti@gmail.com>
2023-08-25 04:25:38 +0530

    Database created for form

--- app.py
+++ app.py
@@ -1,12 +1,45 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import requests
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/diet_body_transformer'
+db = SQLAlchemy(app)
 
-@app.route("/")
+class users(db.Model):
+
+    '''
+    id name height weight age gender email password
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=False)
+    email = db.Column(db.String(70), unique=True)
+    password = db.Column(db.String(64), unique=True)
+    age = db.Column(db.String(3), unique=False)
+    height = db.Column(db.String(6), unique=False)
+    weight = db.Column(db.String(6), unique=False)
+    gender = db.Column(db.String(12), unique=False)
+    email = db.Column(db.String(120), unique=True)
+
+
+@app.route("/", methods = ['GET', 'POST'])
 def home():
-    # return "<p>Hello, World!!!!!</p>"
+    if (request.method == 'POST'):
+        name = request.form.get('name')
+        email = request.form.get('email')
+        height = request.form.get('height')
+        weight = request.form.get('weight')
+        age = request.form.get('age')
+        gender = request.form.get('gender')
+        password_user = request.form.get('password')
+
+        entry = users(name=name, email=email, height=height, weight=weight, age=age, gender=gender, password=password_user)
+        db.session.add(entry)
+        db.session.commit()
+
     return render_template('index.html')
+
+
 # @app.route("/")
 # def home():
 #     # return "<p>Hello, World!!!!!</p>"
